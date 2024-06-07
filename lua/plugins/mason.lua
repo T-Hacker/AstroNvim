319f9ada3faa2cf00a559488073d50d51bff2d97
Micah Halter <micah@mehalter.com>
2024-06-07 09:26:07 -0400

    refactor(plugins): `ensure_installed` tables now extend by default

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -20,9 +20,8 @@ return {
   {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+    opts = {
+      ensure_installed = {
         "stylua",
         -- add more arguments for adding more null-ls sources
       },
